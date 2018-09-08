189e80fa676cd0828269bcdf31d6e4f81ebd1fba
Thai Nguyen <thaiomega3@gmail.com>
2018-09-07 20:35:40 -0500

    Demo Code up to Lecture 5

---
+++ Playingcard/PlayingCard.swift
@@ -0,0 +1,68 @@
+//
+//  PlayingCard.swift
+//  Playingcard
+//
+//  Created by Thai Nguyen on 9/6/18.
+//  Copyright © 2018 Thai Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCard: CustomStringConvertible {
+    
+    var description: String { return "\(rank)\(suit)"}
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        
+        var description: String { return "\(self.rawValue)" }
+        
+        case spades = "♠️"
+        case hearts = "❤️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+        
+        static var all = [Suit.spades, .hearts, .diamonds, .clubs]
+    }
+    
+    enum Rank: CustomStringConvertible {
+        
+        var description: String {
+            switch self {
+            case .ace: return "Ace"
+            case .numeric(let number): return "\(number)"
+            case .face(let kind) where kind == "J": return "Joker"
+            case .face(let kind) where kind == "Q": return "Queen"
+            case .face(let kind) where kind == "K": return "King"
+            default: return "Invalid Rank"
+            }
+        }
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all:[Rank] {
+            var allRanks: [Rank] = [.ace]
+            
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"), .face("Q"), .face("K")]
+            return allRanks
+        }
+    }
+}
